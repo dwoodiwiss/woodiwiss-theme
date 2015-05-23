ae48cb1ec11a05775acecdfb959dfcdba92ef5b6
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2015-05-23 23:20:50 +0100

    add auto prefixing to build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sass        = require('gulp-sass');
-var plumber     = require('gulp-plumber');
-var notify      = require('gulp-notify');
-var Pageres     = require('pageres');
+var gulp         = require("gulp");
+var browserSync  = require("browser-sync");
+var reload       = browserSync.reload;
+var sass         = require("gulp-sass");
+var notify       = require("gulp-notify");
+var Pageres      = require("pageres");
+var autoprefixer = require("gulp-autoprefixer");
 
 // browser-sync task for starting the server.
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function() {
   browserSync({
     proxy: {
       target: "http://localhost:2368",
@@ -21,60 +21,65 @@ gulp.task('browser-sync', function() {
 
 // Sass task, will run when any SCSS files change & BrowserSync
 // will auto-update browsers
-gulp.task('sass', function () {
-  return gulp.src('scss/**/*.scss')
+gulp.task("sass", function () {
+  return gulp.src("scss/**/*.scss")
   .pipe(sass({
-    outputStyle: 'compressed',
+    outputStyle: "compressed",
     errLogToConsole: false,
     onError: function(err) {
       return notify().write(err);
     }
   }))
-  .pipe(gulp.dest('assets/css'))
+  .pipe(autoprefixer({
+    browsers: ["> 5%, last 10 versions"],
+    cascade: false,
+    remove: true
+  }))
+  .pipe(gulp.dest("assets/css"))
   .pipe(reload({stream:true}));
 });
 
 // Reload all Browsers
-gulp.task('bs-reload', function () {
+gulp.task("bs-reload", function () {
   browserSync.reload();
 });
 
-gulp.task('screenshot-desktop', function() {
+gulp.task("screenshot-desktop", function() {
   var pageres = new Pageres({delay: 1})
-  .src('http://localhost:2368', ['1366x768'], {crop: false, filename: '<%= date %>-<%= time %>'})
-  .dest(__dirname + '/resources/progress/desktop');
+  .src("http://localhost:2368", ["1366x768"], {crop: false, filename: "<%= date %>-<%= time %>"})
+  .dest(__dirname + "/resources/progress/desktop");
 
   pageres.run(function (err) {
     if (err) {
       console.log(err);
     } else {
-      console.log('--desktop captured--');
+      console.log("--desktop captured--");
     };
 
   });
 });
 
-gulp.task('screenshot-mobile', function() {
+gulp.task("screenshot-mobile", function() {
   var pageres = new Pageres({delay: 1})
-  .src('http://localhost:2368', ['iphone 5s'], {crop: false, filename: '<%= date %>-<%= time %>'})
-  .dest(__dirname + '/resources/progress/mobile');
+  .src("http://localhost:2368", ["iphone 5s"], {crop: false, filename: "<%= date %>-<%= time %>"})
+  .dest(__dirname + "/resources/progress/mobile");
 
   pageres.run(function (err) {
     if(err) {
       console.log(err);
     } else {
-      console.log('--mobile captured--');
+      console.log("--mobile captured--");
     };
 
   });
 });
 
-gulp.task('screenshots', ['screenshot-desktop', 'screenshot-mobile'], function (){
-  console.log('--all captured--');
+gulp.task("screenshots", ["screenshot-desktop", "screenshot-mobile"], function (){
+  console.log("--all captured--");
 });
 
 // Default task to be run with `gulp`
-gulp.task('default', ['sass', 'browser-sync', 'screenshots'], function () {
-  gulp.watch(['scss/**/*.scss'], ['sass', 'screenshots']);
-  gulp.watch(['*.html', '*.hbs', 'partials/*.html', 'partials/*.hbs', 'js/*.js'], ['bs-reload']);
+gulp.task("default", ["sass", "browser-sync", "screenshots"], function () {
+  gulp.watch(["scss/**/*.scss"], ["sass", "screenshots"]);
+  gulp.watch(["*.html", "*.hbs", "partials/*.html", "partials/*.hbs", "js/*.js"], ["bs-reload"]);
 });
